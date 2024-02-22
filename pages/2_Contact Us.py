eb04f548b2e92fba9b003d4dbf244f211e366678
janjanaika <157570799+janjanaika@users.noreply.github.com>
2024-02-22 17:26:35 +0800

    Add files via upload

---
+++ pages/2_Contact Us.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title('Contact Us')
+
+st.subheader('You can reach out to us if you have any questions through email or social media!')
+st.subheader('Email: onguard.vcis@gmail.com')
+
