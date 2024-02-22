a2fe3b785a52ed250e5ef6986eb5b4de9ca3dbe6
lunalovecode <65585638+lunalovecode@users.noreply.github.com>
2024-02-22 20:53:49 +0800

    Move 1_Home.py

---
+++ 1_Home.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.image("on_guard_logo.jpg", width=400)
+
+st.subheader('We believe that the youth is the hope of the nation and we would like to be part of the solution.')
