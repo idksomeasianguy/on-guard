9ed6e8e75ac17887f976dc10c64ee7e1b384e425
lunalovecode <65585638+lunalovecode@users.noreply.github.com>
2024-02-22 18:04:07 +0800

    Fix 1_Home.py

--- pages/1_Home.py
+++ pages/1_Home.py
@@ -1,15 +1,4 @@
-<<<<<<< HEAD
 import streamlit as st
 
 st.title('On Guard')
 st.subheader('We believe that the youth is the hope of the nation and we would like to be part of the solution.')
-
-
-=======
-import streamlit as st
-
-st.title('On Guard')
-st.subheader('We believe that the youth is the hope of the nation and we would like to be part of the solution.')
-
-
->>>>>>> eb04f548b2e92fba9b003d4dbf244f211e366678
