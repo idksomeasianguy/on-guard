0e0a41e2650fedc317c9c41da7c51837a554812b
lunalovecode <65585638+lunalovecode@users.noreply.github.com>
2024-02-22 22:14:46 +0800

    Update 1_Home.py

--- 1_Home.py
+++ 1_Home.py
@@ -3,3 +3,9 @@ import streamlit as st
 st.image("on_guard_logo.jpg", width=400)
 
 st.subheader('We believe that the youth is the hope of the nation and we would like to be part of the solution.')
+
+st.text("The youth are at risk of exposure to inappropriate content, leaking sensitive information, talking to strangers with malicious intentions, and being too dependent on social media. Researchers also shared that young individuals under 25 years old who are victims of cyber bullying are prone to self-harm and suicidal behaviors and another study revealed that due to gadgets’ excessive use, it could lead to emotional problems, familial conflicts and pushing people towards self isolation.")
+
+st.text("The solution? On Guard! On Guard is a message content monitoring app created to assist parents and guardians to be a digital protector of their children in the online world. We aim to keep every Filipino child safe from the potential threats online.")
+
+st.text("On Guard monitors conversations from online platforms to identify which is safe and which contains potential threats such as cyberbullying, explicit content, and online predators. It also detects over 133 different languages entered in our text and screenshot detector. The kids' conversations will remain private. Parents will only receive notifications when threats are detected.")
