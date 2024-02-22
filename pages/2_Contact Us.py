815a7173a20c0d5fcc5aad33eb58325af6afec18
lunalovecode <65585638+lunalovecode@users.noreply.github.com>
2024-02-22 18:03:26 +0800

    Fix 2_Contact Us.py

--- pages/2_Contact Us.py
+++ pages/2_Contact Us.py
@@ -1,17 +1,6 @@
-<<<<<<< HEAD
 import streamlit as st
 
 st.title('Contact Us')
 
 st.subheader('You can reach out to us if you have any questions through email or social media!')
 st.subheader('Email: onguard.vcis@gmail.com')
-
-=======
-import streamlit as st
-
-st.title('Contact Us')
-
-st.subheader('You can reach out to us if you have any questions through email or social media!')
-st.subheader('Email: onguard.vcis@gmail.com')
-
->>>>>>> eb04f548b2e92fba9b003d4dbf244f211e366678
