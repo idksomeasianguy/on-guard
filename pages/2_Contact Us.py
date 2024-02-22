f9e255e344778a83ecf7023ce808df8537ed96f3
lunalovecode <65585638+lunalovecode@users.noreply.github.com>
2024-02-22 20:11:21 +0800

    Update 2_Contact Us.py

--- pages/2_Contact Us.py
+++ pages/2_Contact Us.py
@@ -1,5 +1,7 @@
 import streamlit as st
 
+st.image("on_guard_logo.jpg", width=400)
+
 st.title('Contact Us')
 
 st.subheader('You can reach out to us if you have any questions through email or social media!')
