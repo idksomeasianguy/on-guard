7326d3ec79be9af0908633c5ce402ef515142da1
lunalovecode <65585638+lunalovecode@users.noreply.github.com>
2024-02-22 18:03:41 +0800

    Fix 3_About Us.py

--- pages/3_About Us.py
+++ pages/3_About Us.py
@@ -1,15 +1,5 @@
-<<<<<<< HEAD
 import streamlit as st
 
 st.title('About Us')
 st.subheader('On Guard is a parental control app created to assist parents and guardians to be a digital protector of their children in the online world.')
 st.subheader('We aim to keep every Filipino child safe from the potential threats online and empower parents to their highest.')
-
-=======
-import streamlit as st
-
-st.title('About Us')
-st.subheader('On Guard is a parental control app created to assist parents and guardians to be a digital protector of their children in the online world.')
-st.subheader('We aim to keep every Filipino child safe from the potential threats online and empower parents to their highest.')
-
->>>>>>> eb04f548b2e92fba9b003d4dbf244f211e366678
