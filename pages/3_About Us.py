edb6a5c28273b296f343c1c83deafb4fb8dcb472
lunalovecode <65585638+lunalovecode@users.noreply.github.com>
2024-02-22 20:11:13 +0800

    Update 3_About Us.py

--- pages/3_About Us.py
+++ pages/3_About Us.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+st.image("on_guard_logo.jpg", width=400)
 st.title('About Us')
 st.subheader('On Guard is a parental control app created to assist parents and guardians to be a digital protector of their children in the online world.')
 st.subheader('We aim to keep every Filipino child safe from the potential threats online and empower parents to their highest.')
