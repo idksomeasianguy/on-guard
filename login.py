f0290d79d166fb78b47099a6dfcec5b4a6450944
Jana Annika Uy <janapie@gmail.com>
2024-02-22 17:48:34 +0800

    Design the front end

--- login.py
+++ login.py
@@ -21,3 +21,14 @@ with st.form(key="login", clear_on_submit=True):
         
         if username == "TeamVCIS" and password == "victory":
             switch_page("monitor")
+
+page_element="""
+<style>
+[data-testid="stAppViewContainer"]{
+  background-image: url("https://cdn.discordapp.com/attachments/1148881767957270538/1210114620019187712/Gradient_Blue_Background.jpg?ex=65e96210&is=65d6ed10&hm=0b9e2039117f4cf088cc5afabb93366f7815af98dfacf93972f0db3c9e76d93a&");
+  background-size: cover;
+}
+</style>
+"""
+
+st.markdown(page_element, unsafe_allow_html=True)
---
+++ pages/1_Home.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title('On Guard')
+st.subheader('We believe that the youth is the hope of the nation and we would like to be part of the solution.')
+
+
